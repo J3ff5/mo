662ae83f6d86495c42be1ddeb3fc5f148e1da53b
Elohdin <jeffersonsoaresbh@gmail.com>
2021-10-08 20:21:36 -0300

    fodas

--- src/App.js
+++ src/App.js
@@ -1,47 +1,6 @@
-import React, {useState, useEffect} from 'react'
-
-import {
-  Container,
-  TextContainer,
-} from './styles'
+import React from "react";
+import Routes from './Routes'
 
 export default function App() {
-
-  const [count, setCount] = useState(0)
-
-  const a = 'Oi meu amor'
-  const b = 'Você é muito especial pra mim'
-  const c = 'Eu te amo'
-  const d = 'Eu te amo muito'
-  const e = 'Eu te amo muito muito'
-  const f = 'Eu te amo muito muito muito'
-  const g = 'A gente vai casar e viver feliz pra sempre'
-  const h = 'Mas eu sou tímido pra falar isso'
-  const i = 'Por isso fiz esse singelo site'
-  const j = 'I love u <3'
-
-  const text = [a, b, c, d, e, f, g, h, i, j]
-
-  function changeText(){
-    const actualText = text[count]
-    if(count < text.length){
-      return actualText
-    } else {
-      setCount(0)
-      return actualText
-    }
-  }
-
-  return (
-    <>
-      <TextContainer>
-        <h1>{changeText()}</h1>
-      </TextContainer>
-
-      <TextContainer>
-        <button onClick={() => setCount(count + 1)}>Me clique</button>
-      </TextContainer>
-    </>
-  );
+  return <Routes />;
 }
-
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -16,3 +16,13 @@ export const TextContainer = styled.div`
   height: 100%;
   background-color: #f5f5f5;
 `;
+
+export const Image = styled.div`	
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    background-color: ${props => props ? props.color : '#f5f5f5'};
+`;
